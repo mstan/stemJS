4eee4954116f8d5d378d46ea8c27c63e2f2ec216
mattsgm <1379tech@gmail.com>
2015-12-02 16:59:20 -0600

    Update ACP handlers for pages and some navigation.

--- admin.js
+++ admin.js
@@ -3,6 +3,8 @@
 ********************************/
 var express = require('express'); 
 var slugHandler = require('./lib/slugHandler.js');
+var getConfigsFromDB = require('./lib/getConfigsFromDB.js');
+var navigationHandler = require('./lib/navigationHandler.js');
 
 /*******************************
 *       Up & Running           *
@@ -26,6 +28,10 @@ admin.get('/', function (req,res) {
 	res.render('sbAdmin/index.ejs');
 });
 
+/*******************************
+*        Routing - Pages       *
+********************************/
+
 //Pull all entries from the database by their slug
 admin.get('/pages', slugHandler.listAllPagesBySlug, function (req,res) {
 	res.render('sbAdmin/pages.ejs');
@@ -48,6 +54,28 @@ admin.post('/pages/edit', slugHandler.updatePageBySlug);
 
 //Remove the entirety of the entry from the database. identified by slug.
 admin.get('/pages/delete/:slug', slugHandler.deletePageBySlug); //end admin.get
+
+/*******************************
+*     Routing - Navigation     *
+********************************/
+
+//Footer
+admin.get('/navigation/footer', getConfigsFromDB.footerConfig, function (req,res) {
+	res.render('sbAdmin/editFooter.ejs');
+});
+admin.post('/navigation/footer/', navigationHandler.updateFooter);
+
+//Sidebar Primary
+admin.get('/navigation/sidebarPrimary', getConfigsFromDB.sidebarPrimaryConfig, function (req,res) {
+	res.render('sbAdmin/editSidebarPrimary.ejs');
+});
+admin.post('/navigation/sidebarPrimary/', navigationHandler.updateSidebarPrimary);
+
+//Header
+admin.get('/navigation/navbar', getConfigsFromDB.navbarConfig, function (req,res) {
+	res.render('sbAdmin/editNavbar.ejs');
+});
+admin.post('/navigation/navbar', navigationHandler.updateNavbar);
 
 
 //Export admin module
